107f2999ac263a7c299d1ac2792c19824bed28d7
lizkavalski <lizkavalski@gmail.com>
2023-06-25 10:42:26 -0700

    makeing sure api key stays hidden

--- src/middleware/video/video.js
+++ src/middleware/video/video.js
@@ -1,8 +1,34 @@
 "use strict";
+const {google}= require('googleapis');
+
+const apiKey= process.env.KEY
+
+
+const youtube = google.youtube({
+  version: 'v3',
+  auth: apiKey
+});
 
 function getVideoID(str) {
   let last = str.substring(str.lastIndexOf("=") + 1, str.length);
+  console.log(last)
   return last;
 }
 
-module.exports = Video;
+
+async function testYouTubeAPI() {
+  try {
+    // Make API requests here
+    const response = await youtube.videos.list({
+      part: 'contentDetails,snippet',
+      id: getVideoID('https://www.youtube.com/watch?v=n61ULEU7CO0'),
+      maxResults: 10
+    });
+    console.log(response.data.items);
+  } catch (error) {
+    console.error('Error testing YouTube API: ', error);
+  }
+}
+// getVideoID('https://www.youtube.com/watch?v=n61ULEU7CO0')
+testYouTubeAPI();
+// module.exports = Video;
